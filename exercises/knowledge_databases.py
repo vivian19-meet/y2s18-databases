ff0dce0e0db7331b0ed6f656b0f4854afc768eeb
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 12:14:02 +0300

    done

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -42,12 +42,32 @@ def edit_article_rating(update_rating,article_name):
     wiki_article=article_name).first()
     know.rating = update_rating
     session.commit()
-
+def top_article():
+    max=0
+    ls = session.query(Knowledge).all()
+    for i in range(5):
+        if(ls[i].rating>max):
+                max=ls[i].rating
+    return max 
+def top_3_articles():
+    ma=[top_article(),0,0]
+    ls = session.query(Knowledge).all()
+    for i in range(5):
+        if(ls[i].rating>ma[1] and ls[i].rating!=top_article()):
+            ma[1]=ls[i].rating
+    for i in range(5):
+        if(ls[i].rating>ma[2] and ls[i].rating!=top_article()and ls[i]!=ma[1].rating):
+            ma[2]=ls[i].rating
+    return ma
 add_article("vivi","canada wiki","canada",8)
-
+add_article("leen","art wiki","art",10)
+add_article("gilad","robotics wiki","robotics",5)
+add_article("alice","phone wiki","phones",7)
 add_article("noam","music wiki","music",9)
 #delete_article_by_topic("canada")
 #delete_all_articles()
 #print(query_article_by_topic("music"))
 edit_article_rating(5,"canada wiki")
 print(query_all_articles())
+print(top_article())
+print(top_3_articles())
