3470d7fac5a15fe6dbc12e14be885a2a47b7773f
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 10:27:01 +0300

    done

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -7,20 +7,47 @@ engine = create_engine('sqlite:///knowledge.db')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
+# v =Knowledge(name,wiki,topic,rating)
 def add_article(name,wiki_article,topic,rating):
-    self=Knowledge(name,wiki_article,topic,)
+    knowledge_object = Knowledge(
+        name=name,
+        wiki_article=wiki_article,
+        topic=topic,
+        rating =rating,
+        )
+    session.add(knowledge_object)
+    session.commit()
+    
 def query_all_articles():
-    pass	
-def query_article_by_topic():
-    pass
+    k = session.query(Knowledge).all()
+    return k
+def query_article_by_topic(their_topic):
+    know = session.query(
+    Knowledge).filter_by(
+    topic=their_topic).first()
+    return know
 
-def delete_article_by_topic():
-    pass
+def delete_article_by_topic(the_topic):
+   session.query(Knowledge).filter_by(
+       topic=the_topic).delete()
+   session.commit()
 
 def delete_all_articles():
-    pass
+    session.query(Knowledge).delete()
+    session.commit()
 
-def edit_article_rating():
-    pass
-add_article(v,"canada wiki")
+def edit_article_rating(update_rating,article_name):
+    know = session.query(
+    Knowledge).filter_by(
+    wiki_article=article_name).first()
+    know.rating = update_rating
+    session.commit()
+
+add_article("vivi","canada wiki","canada",8)
+
+add_article("noam","music wiki","music",9)
+#delete_article_by_topic("canada")
+#delete_all_articles()
+#print(query_article_by_topic("music"))
+edit_article_rating(5,"canada wiki")
+print(query_all_articles())
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -13,13 +13,14 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
-   __tablename__ = 'know'
-   name = Column(String, primary_key=True)
-   wiki_article = Column(String)
-   topic= Column(String)
-   rating = Column(Integer)
-    def __repr__(self):
-    	return ("if you want to learn about: {}\n"
-        	"you should look at the wiki article called: {} \n"
-        	"We gave this article a rating of: {}").format(
-         	self.topic, self.wiki_article, self.rating)
+	__tablename__ = 'know'
+	number = Column(Integer, primary_key=True)
+	name=Column(String)
+	wiki_article = Column(String)
+	topic= Column(String)
+	rating = Column(Integer)
+	def __repr__(self):
+		return ("if you want to learn about: {}\n"
+			"you should look at the wiki article called: {} \n"
+			"We gave this article a rating of: {}").format(
+			self.topic, self.wiki_article, self.rating)
